b491e083ad844c6022b9b197c1f5bac8efe95281
ylong <ducnghia.tin47@gmail.com>
2023-06-13 00:15:46 +0700

    Helper for debuging code

---
+++ ulti/functionCallHelper/functionCallHelper.py
@@ -0,0 +1,63 @@
+import json
+
+class FunctionCallHandler:
+    def __init__(self, functionClassInstance, inputFilePath, outputFilePath):
+        f = open(inputFilePath,'r')
+        data = f.read().split('\n')
+        f.close()
+        self.functionCalls = json.loads(data[0])
+        self.functionCallsInput = json.loads(data[1])
+        f2 = open(outputFilePath,'r')
+        data = f2.read().split('\n')
+        f2.close()
+        self.functionCallsResult = json.loads(data[0])
+        self.instance = LazyCallable(functionClassInstance)
+
+    def simulated(self):
+        out = []
+        for i in range(len(self.functionCalls)):
+            funcName = self.functionCalls[i]
+            funcInput = self.functionCallsInput[i]
+            out.append(self.instance.__call__(funcName, funcInput))
+        return out
+    
+    def simulatedAndCompare(self, verbose = False):
+        for i in range(len(self.functionCalls)):
+            funcName = self.functionCalls[i]
+            funcInput = self.functionCallsInput[i]
+            out = self.instance.__call__(funcName, funcInput)
+            result = self.functionCallsResult[i]
+            if out != result:
+                # checkAgain
+                self.instance.__call__(funcName, funcInput)
+                if verbose:
+                    print(f"Step {i} fail, function {funcName} with input {funcInput} return {out} instead of {result}")
+                break
+        if verbose:
+            print("Simulated done")
+
+class LazyCallable(object):
+    def __init__(self, m_class):
+        self.m_class = m_class
+        self.m_object = None
+
+    def __call__(self, name, k):
+        if self.m_class.__name__ == name:
+            self.m_object = self.m_class(*k)
+            return None
+        
+        function = getattr(self.m_object, name)
+        return function(*k)
+    
+if __name__ == "__main__":
+    class Solution:
+        def __init__(self,name = None):
+            self.name = name
+
+        def add(self, x,y):
+            return x + y
+    
+    fc = FunctionCallHandler(Solution, "ulti/functionCallHelper/test.inp", "ulti/functionCallHelper/test.out")
+    fc.simulatedAndCompare(verbose = True)
+    
+
---
+++ ulti/testHelper/testHelper.py
@@ -0,0 +1,74 @@
+import uuid
+
+
+class TestCase:
+    def __init__(self, testInput, testOutput, testName : str = None, id = None):
+        if not id:
+            self.id = uuid.uuid4()
+        else:
+            self.id = id
+        if not testName:
+            self.name = self.id
+        else:
+            self.name = testName
+        self.input = testInput
+        self.output = testOutput
+
+    def toString(self):
+        if self.name:
+            return self.name
+        return self.id
+
+class Test:
+    def __init__(self):
+        self.testSet = []
+    
+    def addTest(self,  testInput, testOutput, testName = None, testId = None):
+        if not testId:
+            testId = len(self.testSet)
+        newTestCase = TestCase(testInput, testOutput, testName, testId)
+        self.testSet.append(newTestCase)
+        return newTestCase.toString()
+    
+    def addTestCase(self, testCase):
+        self.testSet.append(testCase)
+
+    def checkDefinedTest(self, func):
+        for testCase in self.testSet:
+            out = func(*testCase.input)
+            print(f"Test {testCase.toString()} is {out == testCase.output}")
+
+    def quickTest(self, func, testInput, testOutput, testName = None, addTest = True):
+        if addTest:
+            self.addTest(testInput, testOutput, testName)
+        
+        if not testName:
+            testName = "quickTest"
+        out = func(*testInput)
+        print(f"Test {testName} is {out == testOutput}")
+
+    def quickTestCase(self, func, testCase: TestCase, addTestCase = True):
+        if addTestCase:
+            self.addTestCase(func, testCase)
+        
+        out = func(*testCase.Input)
+        print(f"Test {testCase.name} is {out == testCase.input}")
+
+
+def main():
+    def add(x,y):
+        return x+y
+    test = Test()
+    test.addTest((1,1),2, "Example 1")
+    test.addTest((1,2),3, "Example 2")
+    test.addTest((21,2),23, "Example 3")
+    test.addTest((2001,2),2003)
+    test.addTest((201,2),203)
+    test.checkDefinedTest(add)
+    test.quickTest(add,(1,2),3)
+    test.quickTest(add,(5,2),7)
+    test.quickTest(add,(1,5),6)
+    test.quickTest(add,(6,5),11)
+    
+if __name__=="__main__":
+    main()
