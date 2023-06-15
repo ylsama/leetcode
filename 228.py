b702ec906180c4a4b3a0eb455f88ee09fa5d969a
ylong <ducnghia.tin47@gmail.com>
2023-06-15 22:52:15 +0700

    reupload 228

---
+++ 228.py
@@ -0,0 +1,23 @@
+class Solution:
+    def toRange(self, left, right):
+        if left == right:
+            return f"{left}"
+        return f"{left}->{right}"
+    
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        currentRange = None
+        rangeList = []
+        for i in range(len(nums)+1):
+            if currentRange == None:
+                currentRange = [i,i]
+                continue
+            if i >= len(nums):
+                rangeList.append(self.toRange(*[nums[i] for i in currentRange]))
+                break
+            if nums[i] == nums[i-1] + 1:
+                currentRange[1] = i
+            else:
+                rangeList.append(self.toRange(*[nums[i] for i in currentRange]))
+                currentRange = [i,i]
+                
+        return rangeList
