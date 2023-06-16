4c768549b31e9f7d2f5815b2c592981b7751d245
ylong <ducnghia.tin47@gmail.com>
2023-06-16 19:47:20 +0700

    init 1569. Number of Ways to Reorder Array to Get Same BST

---
+++ 1569.py
@@ -0,0 +1,109 @@
+import json
+from typing import List
+
+from ulti.testHelper.testHelper import TestHelper
+
+
+class BinarySearchTree:
+    def __init__(self, value, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.totalNode = 1
+        self.countWayCache = None
+        if self.left != None:
+            self.totalNode += self.left.totalNode
+        if self.right != None:
+            self.totalNode += self.right.totalNode
+
+    def addNode(self, value):
+        if self.value > value:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.addNode(value)
+
+        if self.value < value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.addNode(value)
+        
+        self.totalNode += 1
+
+    def toDict(self, nestedlevel = None):
+        nextNestedLevel = nestedlevel
+        if nestedlevel != None:
+            nextNestedLevel = nestedlevel - 1
+            if nextNestedLevel < 0:
+                return "..."
+            
+        dictSeft = {}
+        dictSeft["value"] = self.value
+        if self.left != None:
+            dictSeft["left"] = self.left.toDict(nextNestedLevel)
+        if self.right != None:
+            dictSeft["right"] = self.right.toDict(nextNestedLevel)
+        return dictSeft
+
+    def toString(self, nestedlevel = None, indent = 4):
+        dictSeft = self.toDict(nestedlevel)
+        stringSeft = json.dumps(dictSeft, indent = indent)
+        return stringSeft
+
+    def interator(self): 
+        nodes = []
+        nodes.append(self)
+        if self.left != None:
+            nodes.append(*self.left.interator())
+        if self.right != None:
+            nodes.append(*self.right.interator())
+        return nodes
+
+    def countWays(self, dp, modulo):
+        if self.countWayCache != None:
+            return self.countWayCache
+        totalWay = 1
+        if self.left == None and self.right == None:
+            return totalWay
+        if self.left == None:
+            return self.right.countWays(dp, modulo)
+        if self.right == None:
+            return self.left.countWays(dp, modulo)
+        
+        totalWay = self.right.countWays(dp, modulo) * self.left.countWays(dp, modulo) * dp[self.left.totalNode][self.right.totalNode] % modulo
+        self.countWayCache = totalWay
+        return totalWay
+
+class Solution:
+    def numOfWays(self, nums: List[int]) -> int:
+        MODULO = 10**9 + 7
+        root = None
+        for n in nums:
+            if root == None:
+                root = BinarySearchTree(n)
+            else:
+                root.addNode(n)
+        dp = [[0]*(len(nums) + 1) for i in range(len(nums) + 1)]
+
+        for i in range(len(nums)+1):
+            dp[i][0] = 1
+            dp[0][i] = 1
+        for i in range(1,len(nums)+1):
+            for j in range(1,len(nums)+1):
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MODULO
+
+        totalWays = root.countWays(dp, MODULO)
+        return totalWays - 1
+
+def test():
+    a = Solution()
+
+    test = TestHelper()
+
+    test.quickTest(a.numOfWays, [[2,1,3]], 1, "Example 1")
+    test.quickTest(a.numOfWays, [[3,4,5,1,2]], 5, "Example 2")
+    test.quickTest(a.numOfWays, [[1,2,3]], 0, "Example 3")
+
+if __name__ == "__main__":
+    test()
